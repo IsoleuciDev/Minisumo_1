a62b93fb67cfa194472eaeaaa089f3321d4a3afd
Ileana T <98206306+Ileana-B4@users.noreply.github.com>
2023-05-12 21:39:59 +0000

    Add files via upload

---
+++ robot_a1.ino
@@ -0,0 +1,167 @@
+// Control Motores
+int PWMA = 3; 
+int PWMB = 11; 
+//Motores 
+int MOTORA1 = 4; 
+int MOTORA2 = 5;
+int MOTORB1 = 8; 
+int MOTORB2 = 7;
+int STBY = 2; 
+void setup() 
+{
+  Serial.begin(9600);
+  pinMode(MOTORA1, OUTPUT);
+  pinMode(MOTORA2, OUTPUT);
+  pinMode(MOTORB1, OUTPUT);
+  pinMode(MOTORB2, OUTPUT); 
+  pinMode(STBY, OUTPUT);
+  pinMode(PWMA, OUTPUT);
+  pinMode(PWMB, OUTPUT);  
+  digitalWrite(STBY, 1); 
+}
+void loop() 
+{
+  int sensorEI = digitalRead(A0);
+  int sensorC = digitalRead(A1);//
+  int sensorLI = digitalRead(A2);//
+  int sensorED = digitalRead(A3);//
+  int sensorLD = digitalRead(A4);//
+  int sensorlineaD = analogRead(A5); 
+  int sensorlineaI = analogRead(A6);
+  avanzar();
+  
+  if ( sensorlineaD < 490){
+    retroceder();
+    delay(200);
+    giroI();
+    delay(120);
+  }
+  if ( sensorlineaI < 490){
+    retroceder();
+    delay(200);
+    giroD();
+    delay(200);
+  }
+  if ( sensorlineaD < 490  && sensorlineaI  < 490){
+    retroceder();
+    delay(250);
+    giroI();
+    delay(130);
+  }
+  if ( sensorLI ==1){
+    giroDa();
+    delay(1);
+  }
+  if ( sensorLD ==1){
+    giroIa();
+    delay(1);
+  }
+  if ( sensorEI ==1){
+    giroDb();
+    delay(1);
+  }  
+  if ( sensorED ==1){
+    giroIb();
+    delay(1);
+  }
+  
+
+}
+
+void giroDb()
+{
+  digitalWrite(MOTORB1, 1);
+  digitalWrite(MOTORB2, 0);
+  analogWrite(PWMB  , 200);
+  digitalWrite(MOTORA1, 0);
+  digitalWrite(MOTORA2, 1 );
+  analogWrite(PWMA, 200); 
+}
+
+void giroIb()
+{
+  digitalWrite(MOTORB1, 0);
+  digitalWrite(MOTORB2, 1);
+  analogWrite(PWMB  , 200
+  );
+  digitalWrite(MOTORA1, 1);
+  digitalWrite(MOTORA2, 0);
+  analogWrite(PWMA, 200);
+}
+
+void giroDa()
+{
+  digitalWrite(MOTORB1, 1);
+  digitalWrite(MOTORB2, 0);
+  analogWrite(PWMB  , 220);
+  digitalWrite(MOTORA1, 0);
+  digitalWrite(MOTORA2, 0 );
+  analogWrite(PWMA, 0); 
+}
+
+void giroIa()
+{
+  digitalWrite(MOTORB1, 0);
+  digitalWrite(MOTORB2, 0);
+  analogWrite(PWMB  , 0);
+  digitalWrite(MOTORA1, 1);
+  digitalWrite(MOTORA2, 0);
+  analogWrite(PWMA, 220);
+}
+
+void giroD()
+{
+  digitalWrite(MOTORB1, 0);
+  digitalWrite(MOTORB2, 1);
+  analogWrite(PWMB  , 220);
+  digitalWrite(MOTORA1, 1);
+  digitalWrite(MOTORA2, 0 );
+  analogWrite(PWMA, 220); 
+}
+
+void giroI()
+{
+  digitalWrite(MOTORB1, 1);
+  digitalWrite(MOTORB2, 0);
+  analogWrite(PWMB  , 180);
+  digitalWrite(MOTORA1, 0);
+  digitalWrite(MOTORA2, 1);
+  analogWrite(PWMA, 180);
+}
+
+void avanzar()
+{
+  digitalWrite(MOTORB1, 1);
+  digitalWrite(MOTORB2, 0);
+  analogWrite(PWMB  , 200);
+  digitalWrite(MOTORA1, 1);
+  digitalWrite(MOTORA2, 0);
+  analogWrite(PWMA, 200);     
+}
+void avanzarR()
+{
+  digitalWrite(MOTORB1, 1);
+  digitalWrite(MOTORB2, 0);
+  analogWrite(PWMB  , 200);
+  digitalWrite(MOTORA1, 1);
+  digitalWrite(MOTORA2, 0);
+  analogWrite(PWMA, 200);     
+}
+void altoM()
+{
+  digitalWrite(MOTORB1, 0);
+  digitalWrite(MOTORB2, 0);
+  analogWrite(PWMB  , 0);
+  digitalWrite(MOTORA1, 0);
+  digitalWrite(MOTORA2, 0);
+  analogWrite(PWMA, 0);     
+}
+void retroceder()
+{
+  digitalWrite(MOTORB1, 0);
+  digitalWrite(MOTORB2, 1);
+  analogWrite(PWMB  , 255);
+  digitalWrite(MOTORA1, 0);
+  digitalWrite(MOTORA2, 1);
+  analogWrite(PWMA, 255);   
+}
